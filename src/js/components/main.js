8b549f50380b39869ef482c26a050e7e56a5769a
Developer-U <moiseev.yu@yandex.ru>
2022-10-18 11:12:59 +0300

    Add changes to layout

--- src/js/components/main.js
+++ src/js/components/main.js
@@ -1,56 +1,7 @@
 import gsap from "gsap";
 
 
-  window.addEventListener('DOMContentLoaded', function(){    
-  
-   
-  // Добавление адреса доставки в попапе Калькулятор перевозок
-      
-  var addressCalcBtn = document.querySelectorAll('.js-addCalcAddess'); 
-
-  addressCalcBtn.forEach(function(eachaddressCalcBtn){
-    
-    eachaddressCalcBtn.addEventListener('click', function(){       
-      var AddressCalcBlock = document.getElementsByClassName('js-calcGroup');
-      var btnCalcRemove = document.querySelector('.js-removeCalcAddess');      
-      
-      var countD = 1; //Установим счётчик - при клике на кнопку будет добавляться ещё 1 блок отзывов
-      countD += 1;
-      
-      for(let i=1; i<countD; i++) {
-        var newCalcAddress = document.createElement('div');
-        newCalcAddress.classList.add('zayavka-calc__group', 'js-calcGroup', 'row', 'justify-content-between');
-        newCalcAddress.innerHTML = (`
-          <div class="zayavka-calc__side zayavka-calc__side_left ">
-          <fieldset class="row justify-content-between">
-              <legend class="zayavka-calc__legend">Откуда</legend>
-              <input class="service-calc__input zayavka-calc__input service-calc__input_address" type="text" placeholder="Улица">
-              <input class="service-calc__input zayavka-calc__input service-calc__input_house" type="text" placeholder="Дом">
-          </fieldset>
-          </div>
-
-          <div class="zayavka-calc__side zayavka-calc__side_right">
-              <fieldset class="row justify-content-between">
-                  <legend class="zayavka-calc__legend">Куда</legend>
-                  <input class="service-calc__input zayavka-calc__input service-calc__input_address" type="text" placeholder="Улица">
-                  <input class="service-calc__input zayavka-calc__input service-calc__input_house" type="text" placeholder="Дом">
-              </fieldset>
-          </div>
-        `);        
-        document.querySelector('.js-calcTop').append(newCalcAddress);        
-        btnCalcRemove.classList.add('remove-on');
-
-        btnCalcRemove.addEventListener('click', function(){
-    
-          if(AddressCalcBlock.length > 1) {          
-            newCalcAddress.style.display = 'none';
-            btnCalcRemove.classList.remove('remove-on');
-          }          
-        });
-      }
-      
-    }); 
-  });
+window.addEventListener('DOMContentLoaded', function(){    
   
   
 
@@ -62,8 +13,6 @@ import gsap from "gsap";
       scrollbarMaxSize: 300
     }
   });
-
-
 
 
   // Анимация открытия списка городов
@@ -172,38 +121,115 @@ import gsap from "gsap";
   });
 
 
-  // Открытие попапа Заказать машину 
-  var carField = document.querySelector('.for-zakaz');
-  var carFieldOpen = document.querySelectorAll('.js-carOpen'); 
-  var carClose = document.querySelector('.js-carClose'); 
-  var popup = document.querySelector('.popup');
-  var flag = false;  
+    // Открытие попапа Login/Password Логин пароль
 
-  carFieldOpen.forEach(function(carOpen){
-      carOpen.addEventListener('click', function(e){  
-        e.preventDefault();
+    var loginField = document.querySelector('.js-loginPopup');
+    var loginToOpen = document.querySelectorAll('.js-openLogin'); 
+    var loginClose = document.querySelector('.js-loginClose'); 
+    var flag = false;            
+    
+    loginToOpen.forEach(function(openLogBtn){
+      openLogBtn.addEventListener('click', function(e){ 
+        e.preventDefault();     
+        if(!flag) {
+        fadeIn(loginField, 300, 'flex');
+                
+        document.querySelector('body').classList.add('closed');
+  
+        } else {
+        fadeOut(loginField, 300);
         
-          if(!flag) {
-          fadeIn(carField, 300, 'flex');
-            document.querySelector('body').classList.add('closed');
-          } else {
-          fadeOut(carField, 300);
-            document.querySelector('body').classList.remove('closed');          
-          }
-          
-          carClose.addEventListener('click', function(){
-            fadeOut(carField, 300);
-            document.querySelector('body').classList.remove('closed');         
-          });
-
-          popup.addEventListener('click', function(event){
-            if(this == event.target) {
-              fadeOut(carField, 300);
-              document.querySelector('body').classList.remove('closed');              
-            }
-          });                     
+        document.querySelector('body').classList.remove('closed');
+        }
+        
+        loginClose.addEventListener('click', function(){
+          fadeOut(loginField, 300);
+  
+          document.querySelector('body').classList.remove('closed'); 
+        });
+  
+        loginField.addEventListener('click', function(event){
+          if(this == event.target) {
+            fadeOut(loginField, 300);
+  
+            document.querySelector('body').classList.remove('closed');
+          }         
+        });
       });
-  });
+    });
+
+
+    // Добавление тегов
+              
+    var itemTags = document.querySelectorAll('.tags-list li');
+                        
+    var addTagBtn = document.createElement('button'); // Создаём кнопку Добавить           
+
+    addTagBtn.classList.add('tags-list__span', 'col-auto'); // Присваиваем её стили
+
+    addTagBtn.innerText = 'Показать ещё';  // Задаём ей текст
+
+    var parentCont = document.querySelector('.tags-list'); // Обратимся к родителю
+
+   
+
+    if(itemTags.length > 7) {  // Добавляем кнопку, если больше 7
+      parentCont.append(addTagBtn);
+    }
+
+    
+      
+
+      for(let i=7; i<itemTags.length; i++) {
+        itemTags[i].style.display = 'none'; // Скрывааем все статьи, что больше i=1             
+      }
+
+      var countD = 7; //Установим счётчик - при клике на кнопку будет добавляться ещё 1 блок отзывов
+      addTagBtn.addEventListener('click', function(){
+        var itemTags = document.querySelectorAll('.tags-list li');
+
+          countD += 1;
+
+          if(countD <= itemTags.length) {
+              for(let i=0; i<countD; i++) {
+                  itemTags[i].style.display = 'block';
+
+                  // Когда число добавляемых блоков отзывов равняется всему числу блоков, скрываем кнопку
+                  if(countD == itemTags.length) {
+                    addTagBtn.style.display = 'none';
+              }
+              }
+          }
+      });        
+
+    
+
+
+      // Открытие попапа Заказать машину 
+      var carField = document.querySelector('.for-zakaz');
+      var carFieldOpen = document.querySelectorAll('.js-carOpen'); 
+      var carClose = document.querySelector('.js-carClose'); 
+      var popup = document.querySelector('.popup');
+      var flag = false;  
+
+      carFieldOpen.forEach(function(carOpen){
+          carOpen.addEventListener('click', function(e){  
+            e.preventDefault();
+            
+              if(!flag) {
+              fadeIn(carField, 300, 'flex');
+                document.querySelector('body').classList.add('closed');
+              } else {
+              fadeOut(carField, 300);
+                document.querySelector('body').classList.remove('closed');          
+              }
+              
+              carClose.addEventListener('click', function(){
+                fadeOut(carField, 300);
+                document.querySelector('body').classList.remove('closed');         
+              });                 
+          });
+      });
 
 
   // Открытие попапа Калькулятор перевозок
@@ -277,9 +303,14 @@ import gsap from "gsap";
   const searchFieldOpen = document.getElementById('searchOpen');
   const searchFieldClose = document.querySelector('.js-closeSearch');
 
-  var tl2 = gsap.timeline({paused:true}); 
+  var tl2 = gsap.timeline({paused:true});
+  
+  // Ширина активного окна устройства:
+  var availableScreenWidth = window.screen.availWidth;
 
-  tl2.fromTo(searchField, {display: 'none', x: '102%'}, {duration:0.6, ease: "power3.in", display: 'flex', x: 0});
+  if(availableScreenWidth <= '991') {
+      tl2.fromTo(searchField, {display: 'none', transform: 'translateX(202%)'}, {duration:0.6, ease: "power3.in", display: 'flex', transform: 'translateX(0)'});
+  }
 
   searchFieldOpen.onclick = function(){
     tl2.play();
@@ -288,29 +319,9 @@ import gsap from "gsap";
   searchFieldClose.onclick = function(){
     tl2.reverse();
   }
+  
 
-
-  // Табы на главной в разделе Преимущества
-
-    document.querySelectorAll('.advantages__link').forEach(function(pagLink3){
-    pagLink3.addEventListener('click', function(event3){
-      event3.preventDefault();
-      const path3 = event3.currentTarget.dataset.path3;       
-
-      document.querySelectorAll('.advantages__item').forEach(function(item3){
-        item3.classList.remove('item-active');
-      });
-
-      document.querySelector(`[data-item3='${path3}']`).classList.add('item-active');
-
-      document.querySelectorAll('.adv-art').forEach(function(pagArticle){
-        pagArticle.classList.remove('adv-art-active');
-      });
-
-      document.querySelector(`[data-target3="${path3}"]`).classList.add('adv-art-active');
-    });
-
-  });
+    
 
   
   // Появление блоков меню в мобильной версии
@@ -339,224 +350,73 @@ import gsap from "gsap";
     });
   });
 
+  // Функционал прикрепить файл input file
 
-  // Табы на странице Каталог (Услуги)
+  let fields = document.querySelectorAll('.field__file');
+  // console.log(fields);
+  Array.prototype.forEach.call(fields, function (input) {
+  let label = input.nextElementSibling,
+      labelVal = document.querySelector('.field__file-fake').innerText;                
 
-  // Проитерируем кнопки выбора табов
-  document.querySelectorAll('.services-tabs__link').forEach(function(tabsBtn5){
-    tabsBtn5.addEventListener('click', function(event){
-        event.preventDefault();
-
-        // Зададим константу атрибута data-path у кнопок
-        const path5 = event.currentTarget.dataset.path5;
-
-        // Проитерируем все ссылки и при клике снимем все активные значения
-        document.querySelectorAll('.services-tabs__link').forEach(function(oneTab5){
-          oneTab5.classList.remove('tabs-link-active');
-        });
-
-        // Соответствующей кнопке зададим активное значение
-        document.querySelector(`[data-path5='${path5}']`).classList.add('tabs-link-active');      
-        
-
-        // Итерируем табы и закрываем все открытые табы
-        document.querySelectorAll('.services-article').forEach(function(tabContent){
-          tabContent.classList.remove('services-article-active');
-
-          // Зададим в переменную первый Таб (в стилях делаем первый элемент открытым)        
-          var firstPriceTab = document.querySelector('.services-article:first-of-type');
-
-          // Соответственно при клике на любую кнопку его сразу закрываем
-          firstPriceTab.style.display = 'none';
-
-          // Закинем в переменную текущий Таб с соответствующим атрибутом data-target       
-          var currentTab = document.querySelector(`[data-target5="${path5}"]`);
-
-          // console.log(currentTab.getAttribute('data-target') );
-
-          // console.log(firstPriceTab.getAttribute('data-target') );
-
-          // Зададим условие: если атрибут data-target текущего таба соответствует первому Табу, то есть
-          // если это и есть первый Таб, открываем его, если нет - держим закрытым и открываем соответствующий
-          if(currentTab.getAttribute('data-target5') == firstPriceTab.getAttribute('data-target5')) {
-            firstPriceTab.style.display = 'flex';
-          } else {
-            firstPriceTab.style.display = 'none';
-    
-            currentTab.classList.add('services-article-active');
+      input.addEventListener('change', function (e) {
+          let countFiles = '';
+          if (this.files && this.files.length >= 1)
+          countFiles = this.files.length;
+  
+          if (countFiles) {
+              document.querySelector('.field__file-fake').innerText = 'Файл прикреплён';
+              document.querySelector('.field__file-fake').classList.add('field__file-fake-get');
+              document.querySelector('.field__file-button').classList.add('field__file-button-get');
+          }  else {
+              document.querySelector('.field__file-fake').innerText = labelVal;
           }
-        }); 
-    });  
-
-      
-  });
-
-
-  // Код пагинации на странице Цены
-
-  document.querySelectorAll('.js-links').forEach(function(pagLink){
-    pagLink.addEventListener('click', function(event){
-      event.preventDefault();
-      const path2 = event2.currentTarget.dataset.path2;
-
-      document.querySelectorAll('.js-links').forEach(function(oneLink){
-        oneLink.classList.remove('black');
-      });
-
-      document.querySelector(`[data-path2='${path2}']`).classList.add('black');
-
-      document.querySelectorAll('.article-rew').forEach(function(pagArticle){
-        pagArticle.classList.remove('article-rew-active');
-      });
-
-      document.querySelector(`[data-target2="${path2}"]`).classList.add('article-rew-active');
-    });
-
-  });
-
-
-  // Табы на странице Цены
-
-  // Проитерируем кнопки выбора табов
-  document.querySelectorAll('.js-priceTab').forEach(function(tabsBtn6){
-    tabsBtn6.addEventListener('click', function(event){
-        event.preventDefault();
-
-        const path6 = event.currentTarget.dataset.path6;
-
-        // Проитерируем все ссылки и при клике снимем все активные значения
-        document.querySelectorAll('.js-priceTab').forEach(function(oneTab6){
-          oneTab6.classList.remove('active');     
-        });
-
-        // Соответствующей кнопке зададим активное значение
-        document.querySelector(`[data-path6='${path6}']`).classList.add('active');      
-      
-
-        // Итерируем табы и закрываем все открытые табы
-        document.querySelectorAll('.price-art').forEach(function(tabContent){
-          tabContent.classList.remove('price-art-active');
-
-          // Зададим в переменную первый Таб (в стилях делаем первый элемент открытым)        
-          var firstPricesTab = document.querySelector('.price-art:first-of-type');
-
-          // Соответственно при клике на любую кнопку его сразу закрываем
-          firstPricesTab.style.display = 'none';
-
-          // Закинем в переменную текущий Таб с соответствующим атрибутом data-target       
-          var currentTab = document.querySelector(`[data-target6="${path6}"]`);
-
           
-          if(currentTab.getAttribute('data-target6') == firstPricesTab.getAttribute('data-target6')) {
-            firstPricesTab.style.display = 'block';
-          } else {
-            firstPricesTab.style.display = 'none';
-
-            currentTab.classList.add('price-art-active');
-          }
-        });
-    });     
+      });
   });
 
 
+  
 
- 
+
 
 
 
   // Ширина активного окна устройства:
-  var availableScreenWidth = window.screen.availWidth;
+//   var availableScreenWidth = window.screen.availWidth;
 
-  if(availableScreenWidth < '992') {  // Зададим условие что функция будет работать только на мобильных
+//   if(availableScreenWidth < '992') {  // Зададим условие что функция будет работать только на мобильных
 
-    window.onload = function() {    
-    var letters = document.getElementsByClassName('reviews-letters__image')
-      ,letterBtn = document.querySelector('.reviews-letters__btn');
+//     window.onload = function() {    
+//     var letters = document.getElementsByClassName('reviews-letters__image')
+//       ,letterBtn = document.querySelector('.reviews-letters__btn');
 
-    // console.log(letters.length);
+//     // console.log(letters.length);
 
-    for(let i=3; i<letters.length; i++) {
-      letters[i].style.display = 'none';
-    }
+//     for(let i=3; i<letters.length; i++) {
+//       letters[i].style.display = 'none';
+//     }
 
-    var countD = 3; //Установим счётчик - при клике на кнопку будет добавляться по 3 письма
-      letterBtn.addEventListener('click', function(){
-        var letters = document.getElementsByClassName('reviews-letters__image');
+//     var countD = 3; //Установим счётчик - при клике на кнопку будет добавляться по 3 письма
+//       letterBtn.addEventListener('click', function(){
+//         var letters = document.getElementsByClassName('reviews-letters__image');
 
-        countD += 3;
+//         countD += 3;
 
-        if(countD <= letters.length) {
-          for(let i=0; i<countD; i++) {
-            letters[i].style.display = 'block';
-          }
-        }
-      });    
-    }
+//         if(countD <= letters.length) {
+//           for(let i=0; i<countD; i++) {
+//             letters[i].style.display = 'block';
+//           }
+//         }
+//       });    
+//     }
 
-  } else {
-    var letterBtn = document.querySelector('.reviews-letters__btn');
-    letterBtn.style.display = 'none';
-  }
+//   } else {
+//     var letterBtn = document.querySelector('.reviews-letters__btn');
+//     letterBtn.style.display = 'none';
+//   }
+
+  
 });
 
 
-  
-  
-
-
-  jQuery (function($) { 
-
-    // Ползунок цены в калькуляторе
-
-    // $("#range").ionRangeSlider({
-    //   min: 0,
-    //   max: 377,
-    //   from: 60
-    // });
-
-    // Аккордеоны  
-
-    $( "#accordion" ).accordion({
-      collapsible: true,  
-      active:1,   
-      heightStyle: 'content',
-      animate: 700
-    });
-
-
-    // Аккордеоны на странице вакансии
-    
-    $( "#accordion2" ).accordion({
-      collapsible: true, 
-      active:1,   
-      heightStyle: 'content',     
-	    header: '> .accordion-item > .accordion-header',
-      animate: 700
-    });
-
-    $( "#accordion3" ).accordion({
-      collapsible: true, 
-      active:false,        
-      heightStyle: 'content',     
-	    header: '> .accordion-item > .accordion-header',
-      animate: 700
-    });
-
-    $( "#accordion4" ).accordion({
-      collapsible: true, 
-      active:false,     
-      heightStyle: 'content',     
-	    header: '> .accordion-item > .accordion-header',
-      animate: 700
-    });
-
-    $( "#accordion5" ).accordion({
-      collapsible: true, 
-      active:1,   
-      heightStyle: 'content',     
-	    header: '> .accordion-item > .accordion-header',
-      animate: 700
-    });
-
-  });
 
